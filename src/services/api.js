0bbdef0c4ced66347e87e1dd290c55ce45ad6c9c
Pedro Henrique <53953087+peedrinhoph@users.noreply.github.com>
2023-11-23 11:16:09 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://172.31.0.41:3333/api'
+    //baseURL: 'http://0.0.0.0:3333/api'
     baseURL: `http://${process.env.REACT_APP_IP_API}:${process.env.REACT_APP_PORTA_API}/api`
 });
 
